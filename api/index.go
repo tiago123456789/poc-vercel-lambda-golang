c8369a6e30b80151df6ae0bae1025ba8bc323fe1
Tiago R. da costa <tiagorosadacost@gmail.com>
2024-09-01 20:49:07 -0300

    send code updated to remote repository

--- api/index.go
+++ api/index.go
@@ -1,11 +1,23 @@
 package handler
 
-
 import (
-	"fmt"
+	"json"
 	"net/http"
 )
-   
+
+type Person struct {
+	Name string `json:"name"`
+	Email string `json:"email"`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	var person Person
+	err := json.NewDecoder(r.Body).Decode(&person)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(person)
 }

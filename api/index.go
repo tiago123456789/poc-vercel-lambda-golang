699321edf42c138435cbc9c84fe1bd0f8d741c17
Tiago R. da costa <tiagorosadacost@gmail.com>
2024-09-01 21:17:54 -0300

    change code to test if vercel function using golang can send 100 request concurrently

--- api/index.go
+++ api/index.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
+	"sync"
 )
 
 type Person struct {
@@ -10,7 +12,40 @@ type Person struct {
 	Email string `json:"email"`
 }
 
+const URL = "https://webhook-test.com/63b50b32173a09a2216386b1c9a8374d"
+
+func makePostRequest(data interface{}, wg *sync.WaitGroup) error {
+	body, err := json.Marshal(data)
+	if err != nil {
+		return err;
+	}
+
+	r, err := http.NewRequest("POST", URL, bytes.NewBuffer(body))
+	if err != nil {
+		return err;
+	}
+
+	r.Header.Add("Content-Type", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(r)
+	if err != nil {
+		return err;
+	}
+	
+	defer func() {
+		res.Body.Close()
+		wg.Done()
+	}()
+	return nil
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("405 - Method not implemented!"))
+        return
+	}
+
 	var person Person
 	err := json.NewDecoder(r.Body).Decode(&person)
     if err != nil {
@@ -18,6 +53,14 @@ func Handler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i ++ {
+		wg.Add(1)
+		go makePostRequest(person, &wg)
+	}
+
+	wg.Wait()
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(person)
 }
